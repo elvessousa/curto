8f5b5be9a97decc1eafcbc7df9cdd70a8b976478
Elves Sousa <elves.sousa@gmail.com>
2021-05-20 18:28:18 -0300

    Reports screen data

--- curto/shortener/admin.py
+++ curto/shortener/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
-from curto.shortener.models import UrlRedirect
+from curto.shortener.models import UrlRedirect, UrlLog
 
 
 @admin.register(UrlRedirect)
 class UrlRedirectAdmin(admin.ModelAdmin):
+    """ Shows and creates redirections on Admin """
     list_display = ('target', 'slug', 'created_at', 'updated_at')
+
+
+@admin.register(UrlLog)
+class UrlLogAdmin(admin.ModelAdmin):
+    """ Shows data from each redirection access """
+    list_display = ('origin', 'created_at', 'user_agent',
+                    'host', 'ip', 'url_redirect')
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
--- curto/shortener/views.py
+++ curto/shortener/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.db.models import Count
+from django.db.models.functions import TruncDate
 
-from curto.shortener.models import UrlRedirect
+from curto.shortener.models import UrlRedirect, UrlLog
 
 
 def redirection(request, slug):
     """ Redirection """
 
     url_redirect = UrlRedirect.objects.get(slug=slug)
+
+    # Add redirection info to UrlLog
+    UrlLog.objects.create(
+        origin=request.META.get('HTTP_REFERER'),
+        user_agent=request.META.get('HTTP_USER_AGENT'),
+        host=request.META.get('HTTP_HOST'),
+        ip=request.META.get('REMOTE_ADDR'),
+        url_redirect=url_redirect
+    )
 
     return redirect(url_redirect.target)
 
@@ -17,9 +28,22 @@ def reports(request, slug):
 
     url_redirect = UrlRedirect.objects.get(slug=slug)
     site_url = request.build_absolute_uri(f'/{slug}')
+
+    redirects_by_date = list(
+        UrlRedirect.objects.filter(
+            slug=slug
+        ).annotate(
+            date=TruncDate('logs__created_at')
+        ).annotate(
+            clicks=Count('date')
+        ).order_by('date')
+    )
+
     context = {
         'reduce': url_redirect,
-        'url': site_url
+        'url': site_url,
+        'redirects_by_date': redirects_by_date,
+        'total_clicks': sum(r.clicks for r in redirects_by_date)
     }
 
     return render(request, 'shortener/reports.html', context)
